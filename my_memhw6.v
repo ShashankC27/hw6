ec2037ca6b74b053d489dadef279423e7d1efff9
ShashankC27 <125625115+ShashankC27@users.noreply.github.com>
2023-04-13 20:58:27 -0700

    Add files via upload

--- my_memhw6.v
+++ my_memhw6.v
@@ -2,19 +2,19 @@
 
 //`include "my_mem_interface.sv"
 
-module my_memhw6(my_mem_interface mem_inf);
+module my_memhw6(my_mem_interface mem_in);
 
    // Declare a 9-bit associative array using the logic data type
   bit [8:0] mem_array [int];
   
-   always @(posedge mem_inf.clk) begin
-      if (mem_inf.write) begin
-        mem_array[mem_inf.address] = calc_even_parity(.number(mem_inf.data_in));
+   always @(posedge mem_in.clk) begin
+      if (mem_in.write) begin
+        mem_array[mem_in.address] = calc_even_parity(.number(mem_in.data_in));
       //$display("%b Comparing data %b",{^data_in, data_in},calc_even_parity(data_in));
       //$display("%h Comparing data %h",{^data_in, data_in},calc_even_parity(data_in));
       end
-      else if (mem_inf.read) begin
-        mem_inf.data_out =  mem_array[mem_inf.address];
+      else if (mem_in.read) begin
+        mem_in.data_out =  mem_array[mem_in.address];
         //$display("called fead address %h %h",address,data_out);
       end
    end
